581e06e94864a59923e7f6b7bee803d0856f927a
Vinícius <viniciuscarmo.contato@gmail.com>
2023-04-13 14:00:17 -0300

    Correção de código por falha do merge

--- ObjetosEClasses/Carro.java
+++ ObjetosEClasses/Carro.java
@@ -3,7 +3,7 @@ package ObjetosEClasses;
 import java.util.Scanner;
 
 /**
- * Maim
+ * Main
  */
 public class Main {
     public static void main(String[] args) {
@@ -56,51 +56,7 @@ public class Main {
 /**
  * Carro
  */
- public class Carro {
-    //métodos obrigatórios
-    private int numPortas;
-    private String numChassi;
-    private int anoFabricacao;
-    private String combustivel;
-
-    //métodos opcionais
-    private String cor;
-    private String modelo;
-    private boolean automatico;
-    private boolean arCondicionado;
-
-    public Carro (int numPortas, String numChassi, int anoFabricacao, String combustivel) {
-        this.numPortas = numPortas;
-        this.numChassi = numChassi;
-        this.anoFabricacao = anoFabricacao;
-        this.combustivel = combustivel;
-    }
-
-    public void setcor(String cor) {
-        this.cor = cor;
-    }
-
-    public void setmodel(String modelo) {
-        this.modelo = modelo;
-    }
-
-    public void setautomatico(boolean automatico) {
-        this.automatico = automatico;
-    }
-
-    public void setarCondicionado(boolean arCondicionado) {
-        this.arCondicionado = arCondicionado;
-    }
-
-}
-
-
-package ObjetosEClasses;
-
-/**
- * Carro
- */
-public class Carro {
+ class Carro {
     //métodos obrigatórios
     private int numPortas;
     private String numChassi;
